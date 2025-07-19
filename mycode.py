6fec042254ae3a5ab8f455895a4d7d0df753af9a
sanskar133 <sanskar.shrivastava@aidetic.in>
2025-07-19 17:48:34 +0530

    new dataset created

--- mycode.py
+++ mycode.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import os 
 data = {"name":["alice","bob","charlie"],"age":[25,60,40],"city":['newyork','la','california']}
+g1 = {"name":"riya","age":27,"city":"delhi"}
+
 df = pd.DataFrame(data)
+df.loc[len(df)] = g1
 data_dir = "data"
 os.makedirs(data_dir,exist_ok = True)
 ss_path = os.path.join(data_dir,"sample.csv")
